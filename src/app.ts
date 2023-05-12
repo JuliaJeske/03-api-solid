cec27c57233e796cfffd20770c50e7e61f4109f9
juliajeske <julia.jeske@wehandle.com.br>
2023-05-12 08:41:51 -0300

    feat:Caso de Uso e Design Patterns - Criação de um usuário

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,23 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { prisma } from './lib/prisma'
+import { z } from 'zod'
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (req, reply) => {
+  const registerBodySchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'julia',
-    email: 'teste.@email.com',
-  },
+  const { name, email, password } = registerBodySchema.parse(req.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+  return reply.status(201).send()
 })
